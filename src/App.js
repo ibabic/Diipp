5d011d8b2758aa07801a57397626e58c49ae0a99
Ivan Babic <ibabic@fesb.com>
2019-07-30 13:56:34 +0200

    App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ import { connect } from 'react-redux';
 import * as actions from './store/actions/index';
 
 class App extends Component {
-
   componentDidMount () {
     this.props.onTryAutoSignup();
   }
@@ -26,10 +25,8 @@ class App extends Component {
           <Route path="/login"   component={Login} />
           <Route path="/register"   component={Register} />
           <Route path="/logout"   component={Logout} />
-         
          </Switch>
        </Layout>
-       
       </div>
     );
   }
@@ -40,9 +37,11 @@ const mapStateToProps = state => {
     isAuthenticated: state.auth.token !== null
   };
 };
+
 const mapDispatchToProps = dispatch => {
   return {
     onTryAutoSignup: () => dispatch( actions.authCheckState() )
   };
 };
+
 export default withRouter( connect( mapStateToProps, mapDispatchToProps )( App ) );
